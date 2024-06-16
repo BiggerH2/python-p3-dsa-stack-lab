d4b44892ef408531c19a71f295a922004e506923
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-06-16 21:28:25 +0300

    updated

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,44 @@
 class Stack:
+    def __init__(self, iterable=None, limit=None):
+        self.stack = list(iterable) if iterable else []
+        self.limit = limit
 
-    def __init__(self, items = [], limit = 100):
-        pass
-
-    def isEmpty(self):
-        pass
-
-    def push(self, item):
-        pass
+    def push(self, value):
+        if self.limit is None or len(self.stack) < self.limit:
+            self.stack.append(value)
+        else:
+            raise StackFullException("Stack Overflow - Stack is full")
 
     def pop(self):
-        pass
+        if self.stack:
+            return self.stack.pop()
+        else:
+            raise IndexError("pop from empty stack")
 
     def peek(self):
-        pass
-    
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
+
     def size(self):
-        pass
+        return len(self.stack)
+
+    def empty(self):
+        return len(self.stack) == 0
 
     def full(self):
-        pass
+        if self.limit is not None:
+            return len(self.stack) == self.limit
+        else:
+            return False
 
-    def search(self, target):
-        pass
+    def search(self, value):
+        try:
+            index = self.stack.index(value)
+            return len(self.stack) - index - 1
+        except ValueError:
+            return -1
+
+class StackFullException(Exception):
+    pass
